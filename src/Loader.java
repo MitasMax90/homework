00c771d02ceba28d5ca75530014ef0d5a8723132
MitasMax90 <Rostishka90@mail.ru>
2022-01-03 22:08:27 +0300

    First commit

---
+++ src/Loader.java
@@ -0,0 +1,14 @@
+public class Loader {
+    public static void main(String[] args) {
+        int i = 10;
+        do {
+            System.out.println("Я выполнился!");
+        } while (i < 1) ;
+        i--;
+
+        System.out.println("Value i = " + i);
+    }
+}
+/* Используя цикл do...while, напишите программу, которая уменьшает значение i до тех пор, пока оно больше 1.
+
+В программе должна выполнится хотя бы одна итерация.*/
